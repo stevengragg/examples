a8a5c88a33dfd20c556b740627edaca7908ba0f6
sgragg5619 <sgragg5619@gmail.com>
2022-01-02 04:00:47 +0800

    Remove container

--- tailwindcss/imports/ui/App.js
+++ tailwindcss/imports/ui/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import {Dashboard} from './Dashboard';
-import {List} from './List';
+import React from "react";
+import { Dashboard } from "./Dashboard";
+import { List } from "./List";
 
 const Title = () => (
-  <div className="container flex flex-col justify-center space-y-2 space-x-2 mb-5">
-    <p className="text-3xl text-center m-5">Meteor tailwindcss Example</p>
-    <img src="/logo.svg" className="h-7 sm:h-8"/>
+  <div className="flex flex-col justify-center space-y-2 space-x-2 mb-5 text-center">
+    <p className="text-3xl m-5">Meteor tailwindcss Example</p>
+    <img src="/logo.svg" className="h-7 sm:h-8" />
   </div>
 );
 
 export const App = () => {
   return (
     <div className="divide-y divide-gray-100">
-      <Title/>
-      <Dashboard/>
-      <List/>
+      <Title />
+      <Dashboard />
+      <List />
     </div>
   );
 };
